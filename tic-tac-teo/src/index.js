0152e0eb9a1a09838d1d9e2666eb70a980515dc3
Coldfin Lab <30524626+coldfins@users.noreply.github.com>
2018-11-26 15:40:44 +0530

    Add files via upload
    this is tic-tac-teo game app which is develop in reactjs, redux.

---
+++ tic-tac-teo/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Game from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+<div> Tic Tac Teo </div>
+ReactDOM.render(<Game />, document.getElementById('root'));
+registerServiceWorker();
